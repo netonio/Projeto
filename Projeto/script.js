94040f1821decf2e455d1f1c618c57105419fc60
Wildson-Timoteo <wildson.wil28@gmail.com>
2024-11-12 00:44:37 -0300

    Search bar update

--- Projeto/script.js
+++ Projeto/script.js
@@ -1,78 +1,78 @@
-if(window.location.pathname.endsWith('index.html')) { //O código abaixo só acontecerá na página em que seu caminho termina com index.html
+if (window.location.pathname.endsWith('index.html')) { //O código abaixo só acontecerá na página em que seu caminho termina com index.html
 
     let count = 1; //Criando a variavel count
     let interval;
 
     //Garante que o botão radio1 estará marcado e pronto para ser utilizado
-    document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("DOMContentLoaded", function () {
         document.getElementById('radio1').checked = true;
     });
 
     //Criando função para iniciar o carrossel automático
-    function startInterval(){ 
-        interval = setInterval( function(){
+    function startInterval() {
+        interval = setInterval(function () {
             nextImage();
         }, 5000); //O intervalo é dado em milissegundos, por isso 5000
     }
 
     //Criando função para resetar e reiniciar a contagem
     function resetInterval() {
-            clearInterval(interval); // Limpa o intervalo atual
-            startInterval(); // Inicia um novo intervalo
-        }
+        clearInterval(interval); // Limpa o intervalo atual
+        startInterval(); // Inicia um novo intervalo
+    }
 
     startInterval();
 
     // função para passar as imagens
-    function nextImage(){ 
+    function nextImage() {
         count++
-        if(count>4){ //Se o count for maior que 4, a contagem voltará para 1
+        if (count > 4) { //Se o count for maior que 4, a contagem voltará para 1
             count = 1;
         }
 
-        document.getElementById('radio'+ count).checked = true; //*Muda o botão ativo automaticamente, de acordo com o count
+        document.getElementById('radio' + count).checked = true; //*Muda o botão ativo automaticamente, de acordo com o count
     }
 
     // Adiciona eventos de clique nos botões de rádio
     for (let i = 1; i <= 4; i++) {
-        document.getElementById('radio' + i).addEventListener('click', function() {
+        document.getElementById('radio' + i).addEventListener('click', function () {
             count = i; // Atualiza o count para o botão clicado
             clearInterval(interval);
             resetInterval();
         });
-    }    
+    }
 };
 
 //GARANTE QUE OS ELEMENTOS ESTEJAM DISPONÍVEIS PARA USO
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const root = document.documentElement;
     const tema = localStorage.getItem('tema')
 
-    if(tema === 'escuro'){
+    if (tema === 'escuro') {
         root.style.setProperty('--cor-principal', 'rgb(77, 54, 35)');
         root.style.setProperty('--cor-secundaria', 'rgb(195, 195, 140)');
         root.style.setProperty('--cor-fonte', 'rgb(77, 54, 35)');
-    }else{
+    } else {
         root.style.setProperty('--cor-principal', 'rgb(56, 133, 124)');
         root.style.setProperty('--cor-secundaria', 'rgb(255,255,255)');
         root.style.setProperty('--cor-fonte', 'rgb(0,0,0)');
     }
 });
 
-function mudarTema(){
+function mudarTema() {
 
     const root = document.documentElement;
-    
+
     //Chamando as varíaveis do CSS
     const corPrincipal = getComputedStyle(root).getPropertyValue('--cor-principal').trim();
 
     //Alterando o valor das varíaveis do CSS
-    if(corPrincipal === 'rgb(56, 133, 124)'){
+    if (corPrincipal === 'rgb(56, 133, 124)') {
         root.style.setProperty('--cor-principal', 'rgb(77, 54, 35)');
         root.style.setProperty('--cor-secundaria', 'rgb(195, 195, 140)');
         root.style.setProperty('--cor-fonte', 'rgb(77, 54, 35)');
         localStorage.setItem('tema', 'escuro') // Armazena a preferência
-    }else{
+    } else {
         root.style.setProperty('--cor-principal', 'rgb(56, 133, 124)');
         root.style.setProperty('--cor-secundaria', 'rgb(255,255,255)');
         root.style.setProperty('--cor-fonte', 'rgb(0,0,0)');
@@ -117,7 +117,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-if(window.location.pathname.endsWith('contato.html')) {
+if (window.location.pathname.endsWith('contato.html')) {
     const form = document.getElementById('formulario');
     const campos = document.querySelectorAll('.required');
     const spans = document.querySelectorAll('.span-required');
@@ -130,31 +130,31 @@ if(window.location.pathname.endsWith('contato.html')) {
     }
 
     // Função para desfazer as alterações
-    function removerErro(index){
+    function removerErro(index) {
         campos[index].style.border = ''
         spans[index].style.display = 'none'
     }
 
     // Função para validar o email, chamando a função de erro enqunato a condição não é atendida
     function validarEmail() {
-        if (!emailRegex.test(campos[0].value)){
+        if (!emailRegex.test(campos[0].value)) {
             definirErro(0);
-        } else{
+        } else {
             removerErro(0)
         }
     }
 
     // Função para validar o nome, chamando a função de erro enqunato a condição não é atendida
     function validarNome() {
-        if (campos[1].value.length < 3){
+        if (campos[1].value.length < 3) {
             definirErro(1);
-        } else{
+        } else {
             removerErro(1)
         }
     }
 
     // Adiciona um listener do botão submit
-    form.addEventListener('submit', function(event) {
+    form.addEventListener('submit', function (event) {
         event.preventDefault(); // Impede o envio do formulário
         validarNome(); // Chama a função para validar o nome
         validarEmail(); // Chama a função para validar o email
@@ -170,3 +170,41 @@ if(window.location.pathname.endsWith('contato.html')) {
         }
     });
 };
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Obtendo o elemento de entrada da barra de pesquisa
+    const searchinput = document.getElementById('search');
+
+    // Verificando se o elemento de input foi encontrado
+    if (searchinput) {
+        // Adicionando o evento 'input' ao campo de pesquisa
+        searchinput.addEventListener('input', (event) => {
+            const value = formatString(event.target.value);
+
+            // Selecionando todos os itens da lista
+            const itens = document.querySelectorAll('.items .item');
+
+            itens.forEach(item => {
+                // Exibindo ou ocultando o item com base na pesquisa
+                if (formatString(item.textContent).includes(value)) {
+                    item.style.display = 'flex';
+                    // Caso o item precise ter uma direção de flex coluna
+                    item.style.flexDirection = 'column';
+
+                } else {
+                    item.style.display = 'none';
+
+                }
+            });
+        });
+    } else {
+        console.error("Elemento de input com id 'search' não encontrado.");
+    }
+});
+
+// Função para formatar a string e facilitar a pesquisa
+function formatString(value) {
+    return value
+        .toLowerCase() // Converte para letras minúsculas
+        .trim(); // Remove espaços desnecessários
+}
